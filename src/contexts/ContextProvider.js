6f27c05abd182516c9e190e411bec2dbfc4b6f20
Kenttrann2302 <110959350+Kenttrann2302@users.noreply.github.com>
2022-10-11 17:36:54 -0400

    Add files via upload

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,62 @@
+import React, { createContext, useContext, useState } from 'react'; 
+
+const StateContext = createContext();
+
+const initialState = {
+    chat: false,
+    cart: false,
+    userProfile: false,
+    notification: false,
+};
+
+export const ContextProvider = ({ children }) => {
+    const [activeMenu, setActiveMenu] = useState
+    (true);
+    const [isClicked, setIsClicked] = useState
+    (initialState);
+    const [screenSize, setScreenSize] = useState
+    (undefined); // if the screen size is too small, then the Navbar will not be opened
+    const [currentColor, setCurrentColor] = useState('#03C9D7');
+    const [currentMode, setCurrentMode] = useState('Light');
+    const [themeSettings, setThemeSettings] = useState(false);
+
+    const setMode = (e) => {
+        setCurrentMode(e.target.value);
+        localStorage.setItem('themeMode', e.target.value);
+    };
+
+    const setColor = (color) => {
+        setCurrentColor(color);
+        localStorage.setItem('colorMode', color);
+    }
+
+    const handleClick = (clicked) => {
+        setIsClicked({ ...initialState, [clicked]: 
+        true});
+    } // get the information on which element has been clicked
+
+    return (
+        <StateContext.Provider value={{ 
+            currentColor, currentMode, 
+            activeMenu, 
+            screenSize, setScreenSize, 
+            handleClick, 
+            isClicked, 
+            initialState, 
+            setIsClicked, 
+            setActiveMenu, 
+            setCurrentColor, 
+            setCurrentMode, 
+            setMode, 
+            setColor, 
+            themeSettings, 
+            setThemeSettings 
+        }}>
+            {children} 
+        </StateContext.Provider>
+        // whatever inside the StateContext will be displayed
+    );
+};
+
+export const useStateContext = () => useContext(StateContext);
+// give me the data from the context by using the context and specify the StateContext
